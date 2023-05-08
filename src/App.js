6040f24643e972b11ccc0ea954bb0abad5468a1a
Tiny P Christhudas <tinypchristhudas@gmail.com>
2023-05-08 22:54:46 +0530

    About to go over props

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
 
 import './App.css';
 
+import Employee from "./Components/Employee"
+
 function App() {
+  
+  const showEmployees = true
+  console.log("we are about to list employees")
+
+
   return (
     <div className="App">
-   
+      { console.log("Inside the return ")}
+        
+      {showEmployees ?
+      <>  
+        <Employee/>
+        <Employee/>
+        <Employee/>
+        <Employee/>
+      </>
+      :
+      <p>You are not allowed to view employees</p>
+      } 
     </div>
   );
 }
