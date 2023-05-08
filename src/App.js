708461ca38e35f59439f3c809ecf6a423d943543
Tiny P Christhudas <tinypchristhudas@gmail.com>
2023-05-08 23:27:17 +0530

    getting ready for tailwindcss

--- src/App.js
+++ src/App.js
@@ -2,23 +2,33 @@
 import './App.css';
 
 import Employee from "./Components/Employee"
+import { useState } from 'react';
 
 function App() {
-  
+
   const showEmployees = true
   console.log("we are about to list employees")
-
+   const [role, setRole] = useState()
 
   return (
     <div className="App">
-      { console.log("Inside the return ")}
         
       {showEmployees ?
       <>  
-        <Employee/>
-        <Employee/>
-        <Employee/>
-        <Employee/>
+
+        <input 
+          type="text" 
+          onChange={(e)=> {
+            console.log(e.target.value)
+            setRole(e.target.value)
+          }}
+  
+        />
+
+        <Employee name="Caleb" role="Intern"/>
+        <Employee name="Abby" role = {role}/>
+        <Employee name="John"/>
+      
       </>
       :
       <p>You are not allowed to view employees</p>
